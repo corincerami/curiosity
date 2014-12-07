d64fddd74ca72714e73f4efe24259ca05c8190eb
Chris C Cerami <chrisccerami@gmail.com>
2014-12-07 13:00:33 -0500

    Sanitized form input to protect against SQL injection.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,11 +1,10 @@
 class ImageController < ApplicationController
   def show
-    @images = Image.where(sol: params[:sol])
+    @images = Image.where(:sol => params[:sol])
   end
 
   def index
-    # find the martial solar date of the most recent image taken
-    # @most_recent = Image.all.sort_by { |hash| -hash[:sol].to_i }.first[:sol]
+    # find the martian solar date of the most recent image taken
     @image_count = Image.count
     @most_recent = Image.maximum(:sol)
   end
